cad1f5f9781ae6e78a882b382ae0e95a56d823e1
Rafael Cruz <rcas2009@outlook.com>
2024-02-26 15:13:05 -0300

    feat: rendering templates

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,7 @@
 # Store standard routes for user authentication
 
 # A blueprint has urls defined, way to separate app, views can be defined in multiple files
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # Define the auth blueprint
 auth = Blueprint("auth", __name__)
@@ -9,7 +9,8 @@ auth = Blueprint("auth", __name__)
 # Creating routes for login, logout and sign_up
 @auth.route("/login")
 def login():
-    return "<p>Login</p>"
+    # Renders the template, can pass variables and therefore values
+    return render_template("login.html", text="Testing", user="Pudha", boolean=True)
 
 @auth.route("/logout")
 def logout():
@@ -17,4 +18,4 @@ def logout():
 
 @auth.route("/sign-up")
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
--- website/views.py
+++ website/views.py
@@ -1,7 +1,7 @@
 # Store standard routes for user navigation
 
 # A blueprint has urls defined, way to separate app, views can be defined in multiple files to better organize
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 # Define the views blueprint (views = blueprint name)
 views = Blueprint("views", __name__)
@@ -10,4 +10,5 @@ views = Blueprint("views", __name__)
 # @ -> Decorator
 @views.route("/")
 def home():
-    return "<h1>Test</h1>"
+    # Renders the template, can pass variables and therefore values
+    return render_template("home.html")
